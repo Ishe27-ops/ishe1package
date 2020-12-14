47b59fa1684cd21b2d13c3b4050b4b2abc1236ac
Ishe27-ops <ishemudzengi@gmail.com>
2020-12-14 11:53:59 +0200

    Styling

--- R/printer.R
+++ R/printer.R
@@ -10,9 +10,9 @@
 #' @importFrom tibble data_frame
 #' @importFrom utils head adist
 #' @examples
-#' printer(x =rnorm(5), r = rnorm(5))
-printer = function(r, x){
-  x = data_frame(x = x, r = r)
+#' printer(x = rnorm(5), r = rnorm(5))
+printer <- function(r, x) {
+  x <- data_frame(x = x, r = r)
   print(head(x))
   return(x)
 }
